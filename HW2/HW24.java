6bde77bba1f7208e6907527195ed66593497c843
Alexandr Kliuchnyk <askeysy@ukr.net>
2017-02-09 09:03:51 +0200

    com

---
+++ HW2/HW24.java
@@ -0,0 +1,31 @@
+package HW2;
+
+
+public class HW24 {
+
+    static int[] balances = {1200, 250, 2000, 500, 3200};
+    static String[] ownerNames = {"Jane", "Ann", "Jack", "Oww", "Lane"};
+
+    static String fundBalance(String ownerName,double fund){
+        double balance;
+        int i=0;
+        int isElement=0;
+        while (i<ownerNames.length){
+            if (ownerNames[i]==ownerName){
+                isElement=1;
+                break;
+            }
+            i++;
+        }
+        if (isElement==0){
+            return "Wrong owner!";
+        }
+        else{
+            balance=balances[i]+fund;
+            return ownerName+ " "+balance;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(fundBalance("Oww",100));
+     }
+}
