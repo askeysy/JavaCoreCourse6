49eb68d764be87cd16b6c826aec410bdedd7e793
Alexandr Kliuchnyk <askeysy@ukr.net>
2017-02-15 11:39:17 +0200

    com

--- HW3/Bird.java
+++ HW3/Bird.java
@@ -1,26 +1,14 @@
 package HW3;
 
 public class Bird {
-    static void sing(){
-        System.out.println("I am singing");
-    }
-
-    static void birdName(){
-        System.out.println("I am a Bird");
-    }
-
-    static void fly() {
-        System.out.println("I am flying");
-    }
-
-    static void walk() {
-        System.out.println("I am walking");
+    static void sing(String text){
+        System.out.println(text);
     }
 
     public static void main(String[] args) {
-        Bird.walk();
-        Bird.fly();
-        Bird.sing();
-        Bird.birdName();
+        Bird.sing("I am walking");
+        Bird.sing("I am flying");
+        Bird.sing("I am singing");
+        Bird.sing("I am a Bird");
     }
    }
