cf30d9318b7fc58ca9383fb745facf5bb7a446e3
Alexandr Kliuchnyk <askeysy@ukr.net>
2017-02-15 07:41:36 +0200

    com

---
+++ HW3/Adder.java
@@ -0,0 +1,15 @@
+package HW3;
+
+public class Adder extends Arithmetic{
+    private Integer arg1;
+    private Integer arg2;
+    public Adder(Integer arg1,Integer arg2) {
+        this.arg1=arg1;
+        this.arg2=arg2;
+    }
+
+    boolean check(Integer a, Integer b) {
+        if (a >= b) return true;
+        return false;
+    }
+}
---
+++ HW3/Arithmetic.java
@@ -0,0 +1,14 @@
+package HW3;
+
+public class Arithmetic {
+    static int add(Integer a, Integer b){
+        int sum;
+        sum=a+b;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arithmetic.add(5,40));
+    }
+}
+
