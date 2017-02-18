c742fef57b345c41dc7c614bfc99b8061830ab76
Alexandr Kliuchnyk <askeysy@ukr.net>
2017-02-18 11:52:46 +0200

    com

--- HW3/User.java
+++ HW3/User.java
@@ -89,4 +89,5 @@ public class User {
         int month=Worker.getMonthsOfEmployment();
         Worker.setMonthsOfEmployment(month+addMonth);
     }
+
 }
---
+++ HW4/HW42/EnumCurrency.java
@@ -0,0 +1,8 @@
+package HW4.HW42;
+
+
+public class EnumCurrency {
+    public enum currency{
+        USD, EUR
+    }
+}
