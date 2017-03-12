6e1be8a7ac5e52abbc940b86f5076860a8d5d104
Alexandr Kliuchnyk <askeysy@ukr.net>
2017-03-12 15:47:44 +0200

    com

--- HW4/HW44/User.java
+++ HW4/HW44/User.java
@@ -2,7 +2,7 @@ package HW4.HW44;
 
 import HW4.HW41.Bank;
 
-public class User{
+class User{
     long id;
     String name;
     double balance;
@@ -10,6 +10,62 @@ public class User{
     String companyName;
     int salary;
     Bank bank;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getMonthsOfEmployment() {
+        return monthsOfEmployment;
+    }
+
+    public void setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
 
     @Override
     public String toString() {
---
+++ HW4/HW45/main.java
@@ -0,0 +1,9 @@
+package HW4.HW45;
+
+import HW4.HW44.User;
+
+public class main {
+    User UserBankAmerica;
+
+
+}
