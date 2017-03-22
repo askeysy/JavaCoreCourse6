06e9605ba389c1b8926e5e434d36f43457aad690
Alexandr Kliuchnyk <askeysy@ukr.net>
2017-03-22 13:12:23 +0200

    com

--- HW4/HW45/BankSystemImpl.java
+++ HW4/HW45/BankSystemImpl.java
@@ -13,21 +13,26 @@ public class BankSystemImpl implements BankSystem{
 
     @Override
     public void withdrawOfUser(User user, int amount) {
-
+        double result=user.getBalance()-amount;
     }
 
     @Override
     public void fundUser(User user, int amount) {
-
+        double balance=user.getBalance();
+        user.setBalance(balance+amount);
     }
 
     @Override
     public void transferMoney(User fromUser, User toUser, int amount) {
-
+        double balanceUser1=fromUser.getBalance();
+        fromUser.setBalance(balanceUser1-amount);
+        double balaneUser2=toUser.getBalance();
+        toUser.setBalance(balaneUser2+balanceUser1);
     }
 
     @Override
     public void paySalary(User user) {
-
+        int salaryUSer=user.getSalary();
+        user.setSalary(salaryUSer);
     }
 }
